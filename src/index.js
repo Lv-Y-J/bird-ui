7a7103f96aca8f7b51ed12cb5ed1323a65cae5c4
Smalllong <lixiaolong@birdsh.com>
2018-04-24 17:25:47 +0800

    完善

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import ImageInput from './image-input'
 import SimpleCalendar from './simple-calendar'
+import SelectPopup from './select-popup'
+import TreeTable from './tree-table'
 
 export {
   ImageInput,
   SimpleCalendar,
+  SelectPopup,
+  TreeTable,
 }
